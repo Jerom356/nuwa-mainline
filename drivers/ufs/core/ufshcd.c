fac3ffc3c92c80ad4f13f4fc716182b2125404f6
alfaonyt <alex@pillou.net>
2025-08-22 16:06:55 +0200

    HACK: Disable UFS deep sleep
    This hack is required for now to prevent issues on Xiaomi 13 Pro

--- drivers/ufs/core/ufshcd.c
+++ drivers/ufs/core/ufshcd.c
@@ -10911,7 +10911,7 @@ static int ufshcd_wl_probe(struct device *dev)
 
 	blk_pm_runtime_init(sdev->request_queue, dev);
 	pm_runtime_set_autosuspend_delay(dev, 0);
-	pm_runtime_allow(dev);
+	//pm_runtime_allow(dev);
 
 	return  0;
 }
