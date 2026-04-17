b7c734adc2621badc0053dc31c9c973cf6213769
alfaonyt <alex@pillou.net>
2026-04-17 20:34:48 +0200

    remoteproc: qcom: avoid reserved-memory region request conflicts

--- drivers/remoteproc/qcom_q6v5_pas.c
+++ drivers/remoteproc/qcom_q6v5_pas.c
@@ -623,38 +623,55 @@ static void qcom_pas_pds_detach(struct qcom_pas *pas, struct device **pds, size_
 
 static int qcom_pas_alloc_memory_region(struct qcom_pas *pas)
 {
-	struct resource res;
-	int ret;
+	struct reserved_mem *rmem;
+	struct device_node *node;
 
-	ret = of_reserved_mem_region_to_resource(pas->dev->of_node, 0, &res);
-	if (ret) {
-		dev_err(pas->dev, "unable to resolve memory-region\n");
-		return ret;
+	node = of_parse_phandle(pas->dev->of_node, "memory-region", 0);
+	if (!node) {
+		dev_err(pas->dev, "no memory-region specified\n");
+		return -EINVAL;
 	}
 
-	pas->mem_phys = pas->mem_reloc = res.start;
-	pas->mem_size = resource_size(&res);
-	pas->mem_region = devm_ioremap_resource_wc(pas->dev, &res);
-	if (IS_ERR(pas->mem_region)) {
-		dev_err(pas->dev, "unable to map memory region: %pR\n", &res);
-		return PTR_ERR(pas->mem_region);
+	rmem = of_reserved_mem_lookup(node);
+	of_node_put(node);
+	if (!rmem) {
+		dev_err(pas->dev, "unable to resolve memory-region\n");
+		return -EINVAL;
+	}
+
+	pas->mem_phys = pas->mem_reloc = rmem->base;
+	pas->mem_size = rmem->size;
+	pas->mem_region = devm_ioremap_wc(pas->dev, pas->mem_phys, pas->mem_size);
+	if (!pas->mem_region) {
+		dev_err(pas->dev, "unable to map memory region: %pa+%zx\n",
+			&rmem->base, pas->mem_size);
+		return -EBUSY;
 	}
 
 	if (!pas->dtb_pas_id)
 		return 0;
 
-	ret = of_reserved_mem_region_to_resource(pas->dev->of_node, 1, &res);
-	if (ret) {
-		dev_err(pas->dev, "unable to resolve dtb memory-region\n");
-		return ret;
+	node = of_parse_phandle(pas->dev->of_node, "memory-region", 1);
+	if (!node) {
+		dev_err(pas->dev, "no dtb memory-region specified\n");
+		return -EINVAL;
 	}
 
-	pas->dtb_mem_phys = pas->dtb_mem_reloc = res.start;
-	pas->dtb_mem_size = resource_size(&res);
-	pas->dtb_mem_region = devm_ioremap_resource_wc(pas->dev, &res);
-	if (IS_ERR(pas->dtb_mem_region)) {
-		dev_err(pas->dev, "unable to map dtb memory region: %pR\n", &res);
-		return PTR_ERR(pas->dtb_mem_region);
+	rmem = of_reserved_mem_lookup(node);
+	of_node_put(node);
+	if (!rmem) {
+		dev_err(pas->dev, "unable to resolve dtb memory-region\n");
+		return -EINVAL;
+	}
+
+	pas->dtb_mem_phys = pas->dtb_mem_reloc = rmem->base;
+	pas->dtb_mem_size = rmem->size;
+	pas->dtb_mem_region = devm_ioremap_wc(pas->dev, pas->dtb_mem_phys,
+					      pas->dtb_mem_size);
+	if (!pas->dtb_mem_region) {
+		dev_err(pas->dev, "unable to map dtb memory region: %pa+%zx\n",
+			&rmem->base, pas->dtb_mem_size);
+		return -EBUSY;
 	}
 
 	return 0;
